67bc28297f9daa4f8683bf69cb701d6a97fbfd41
Jirath Arnamnath <ice191913@gmail.com>
2024-03-18 11:44:49 +0700

    Updated app.js

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 
+const tasksRoute = require('./src/routes/tasks');
+
 const express = require('express');
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.use('/tasks', tasksRoute);
+app.use((req, res) => {
+    return res.status(404).send({
+        message: 'Not found.'
+    });
 });
 
 module.exports = app;
